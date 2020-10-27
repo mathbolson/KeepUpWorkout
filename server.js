d02f69b4602084aca351fab92f4c7d07d5a2af2e
Matheus Bolson <mathbolson@hotmail.com>
2020-10-27 13:30:46 -0700

    innital directory

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const morgan = require("morgan");
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+app.listen(8080, function() {
+    console.log('listening on 8080');
+  });
